9d71f6ffeed21797db6740d50fa5621aeb2723de
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-10-17 08:38:21 +0530

    Bug Fix: Added encoding
    Added acceptance of encoding format into the loaders to prevent
    `UniCodeDecode` error.

--- utils/loaders.py
+++ utils/loaders.py
@@ -1,11 +1,12 @@
 import os
 
 # Load data
-def text_data_loader(dir: str):
+def text_data_loader(dir: str, encoding: str = "utf8"):
     """
     Function: Accepts the path of data corpus and loads all data in 1 string
     Args:
         dir (str): Path to the data file 
+        encoding (str) : File encoding format if any, default is utf8
     """
     
     # Validate file dir path
@@ -13,18 +14,25 @@ def text_data_loader(dir: str):
         raise FileNotFoundError("Could not locate director")
     
     # List files in the path
-    elif len(os.listdir(dir)) == 0 or all([not file.endswith(".txt") for file in len(os.listdir(dir))]):
+    elif len(os.listdir(dir)) == 0:
         raise ValueError("Empty directory provided or no `.txt` file present in the directory")
 
     # Else return the text as a string
     else:
         # List the valid files
-        files = [file for file in len(os.listdir(dir)) if file.endswith(".txt")]
+        files = [file for file in os.listdir(dir) if file.endswith(".txt")]
+        if len(files) == 0: 
+            raise ValueError("No file with .txt extension found")
         text = ""
         for file in files:
             # Join filepath to dirpath
             filepath = os.path.join(dir, file)
-            with open(filepath) as f:
-                text = text + ".\n" + f.read()
+
+            try:
+                with open(filepath, encoding=encoding) as f:
+                    file_text = f.read()
+                    text = text + "\n" + file_text
+            except Exception as e:
+                raise RuntimeError("Error Reading the file check for the encoding format.")
 
         return text
