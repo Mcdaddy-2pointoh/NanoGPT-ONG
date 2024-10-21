03a01a310694b908dc2052917f5759d545758861
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-10-21 08:48:25 +0530

    Added naive model trainer
    Created a naive model trainer that accepts the optimizer, model, batch_size, block_size and steps to train our simple bigram model.

---
+++ utils/trainer.py
@@ -0,0 +1,40 @@
+# Imports
+import torch
+from utils.augmenters import batch_generator
+from tqdm import tqdm
+def naive_trainer(data: torch.Tensor, model: torch.nn.Module, optimizer, batch_size: int, block_size: int, steps: int = 100):
+    # Validate dtypes
+    if type(data) != torch.Tensor:
+        try:
+            data = torch.tensor(data=data)
+        except Exception as e:
+            raise ValueError(f"Could not convert data of type {type(data)} to torch.Tensor")
+    
+    elif not isinstance(model, torch.nn.Module):
+        raise TypeError("Model must be an instance of torch.nn.Module")
+    
+    elif not isinstance(steps, int):
+        try:
+            steps = int(steps)
+        except Exception as e:
+            raise ValueError("Steps must by of type `int`, could not covenrt steps to type int") from e
+    
+    losses_at_step = []
+
+    for step in tqdm(range(steps)):
+
+        # Sample a batch of data
+        try:
+            xb, yb = batch_generator(data=data, block_size=block_size, batch_size=batch_size)
+        except Exception as e:
+            raise RuntimeError("Could not generate a batch from data.") from e
+        
+        # Evaluate the loss 
+        logits, loss = model(xb, yb)
+        optimizer.zero_grad(set_to_none=True)
+        loss.backward()
+        optimizer.step()
+        losses_at_step.append(loss.item())
+
+    return model, losses_at_step   
+        
