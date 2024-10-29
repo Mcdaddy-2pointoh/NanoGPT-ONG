75a0a672df34a8707920e62ab60de7d9d9ff4b25
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-10-29 12:58:18 +0530

    Visualizer to vizualise losses over steps

---
+++ utils/visualiser.py
@@ -0,0 +1,28 @@
+from matplotlib import pyplot as plt
+import os
+import numpy as np
+
+def plot_loss(losses, dir_path, smoothen=False):
+    """
+    Function: Plots and saves the loss as an image per run
+    Args:
+        losses(list | torch.Tensor) : list of loss per step
+        dir_path(str) : to save the image 
+    """
+    # Check if directory exists at path 
+    if not os.path.isdir(dir_path):
+        raise FileNotFoundError(f"No directory found at {dir_path}")
+    else:
+        run_number = str(len(os.listdir(path=dir_path)) + 1).zfill(4)
+
+    # If smoothen
+    if smoothen:
+        losses = estimate_loss(losses=losses)
+
+    try: 
+        plt.plot(list(range(1,len(losses)+1)), losses)
+        plt.savefig(os.path.join(dir_path, f"loss-run-{run_number}.png"))
+
+    except Exception as e:
+        raise RuntimeError("Could not save loss curve") from e
+    
