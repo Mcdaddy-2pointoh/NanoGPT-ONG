950f72779ec78e1884b41da0756ce1cf038d42fe
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-10-31 10:27:26 +0530

    REFACTOR: `self_attention.py` -> `self_attention_fns.py`
    Renamed the individual components made to explore self attention to `self_attention_fns.py` as this only contains the core operation of masked self attention.

--- utils/modelling/attention/self_attention_fns.py
+++ utils/modelling/attention/self_attention_fns.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 
+
 # Version 1: Averaging
 def self_attention_v1(x):
     """
@@ -102,3 +103,52 @@ def self_attention_v3(x):
     x_bow_3 = wei @ x
 
     return x_bow_3
+
+# Version 4: Self Attention
+def self_attention_v4(x, head_size = 16):
+    """
+    Function: Data dependent token affinity over a matrix instead of a eqi-weighted aggregation
+    Args:
+        x (tensor): A tensor with 3 dimesions i.e. batch, timestep, channels
+    """   
+    # Determine the device
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Convert x
+    if type(x) != torch.Tensor:
+        x = torch.tensor(x).to(device=device)
+    
+        if not len(x.shape) == 3:
+            raise ValueError("Expected tensor of 3 dimensions")
+        
+    else:
+        x = x.to(device=device)
+
+    # Get shape of x
+    B, T, C = x.shape
+
+    # Defining standard params
+    key = torch.nn.Linear(C, head_size, bias=False).to(device=device)
+    query = torch.nn.Linear(C, head_size, bias=False).to(device=device)
+    value = torch.nn.Linear(C, head_size, bias=False).to(device=device)
+
+    # Get the query and the key value for all tokens in X i.e. at all timesteps (B, T)
+    k = key(x)
+    q = query(x)
+
+    # The weight now is computed as the matrix product of q & k
+    wei = q @ k.transpose(-2, -1) # The shape of mat mul is (B, T, 16) @ (B, 16, T) ---> (B, T, T)
+
+    # Creating a masked tensor so that we only get previous token information
+    tril = torch.tril(torch.ones(T,T)).to(device=device)
+    wei = wei.masked_fill(tril==0, float("-inf"))
+
+    # Applying softmax to the last row level dimension to scale "-inf" to 0 
+    wei = F.softmax(wei, dim=-1)
+
+    # Calculating the resultant attention block
+    v = value(x)
+    out = wei @ v
+
+    return out
+
