cda05a3172fe5e30bab6765ae9196dd2532e28a8
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-11-07 15:48:05 +0530

    Integrated `tiktokenizer` into the inference pipeline

--- utils/pipelines/inference.py
+++ utils/pipelines/inference.py
@@ -1,5 +1,6 @@
 # Imports
 from utils.tokenizers.naive_tokenizer import naive_tokenizer
+from utils.tokenizers.tiktokenizer import tiktokenizer
 from utils.modelling.models import LanguageModel
 import torch
 import json
@@ -10,7 +11,7 @@ class InferencePipeline:
     Class: Inference pipeline to run the language model 
     """
 
-    def __init__(self, run_dir: str = None, tokenizer_type: str = "naive", device: str = "cpu"):
+    def __init__(self, run_dir: str = None, device: str = "cpu"):
         """
         Function: Accepts an input string and produces an output from Language model
         Args:
@@ -20,17 +21,23 @@ class InferencePipeline:
 
         # Get a list of all possible devices
         devices = [f'cuda:{i}' for i in range(torch.cuda.device_count())] + ['cpu']
+        
+        # Get model params as dictionary
+        with open(os.path.join(run_dir, "model", "params.json")) as f:
+            model_params = json.load(f)
+
+        self.model_params = model_params
 
         # Try to instance the tokenizer
-        if tokenizer_type not in ["naive", "tiktoken"]:
-            raise ValueError(f"Argument tokenizer_type must be 'naive' or 'tiktoken', found {tokenizer_type}")
+        if model_params['tokenizer_type'] not in ["naive", "tiktoken"]:
+            raise ValueError(f"Value model_params['tokenizer_type'] must be 'naive' or 'tiktoken', found {model_params['tokenizer_type']}")
         
         # Raise error if `naive` tokenizer is the type and the encoding and decoding hashmaps are not present
-        elif tokenizer_type == "naive" and not set(os.listdir(os.path.join(run_dir, "tokenizer"))).issuperset(set(['characters.json', 'decoding_hashmap.json', 'encoding_hashmap.json'])):
+        elif model_params['tokenizer_type'] == "naive" and not set(os.listdir(os.path.join(run_dir, "tokenizer"))).issuperset(set(['characters.json', 'decoding_hashmap.json', 'encoding_hashmap.json'])):
             raise FileNotFoundError("To use naive tokenizer 'characters.json', 'decoding_hashmap.json'& 'encoding_hashmap.json' are needed for initialising the tokenizer")
         
         # Load hashmaps of encoding and decoding to initialise a transformer
-        elif tokenizer_type == "naive":
+        elif model_params['tokenizer_type'] == "naive":
 
             # Load hashmaps
             with open(os.path.join(run_dir, "tokenizer", 'encoding_hashmap.json')) as f:
@@ -43,42 +50,62 @@ class InferencePipeline:
                 characters = json.load(f)
 
             # Load a tokenizer
-            self.tokenizer = naive_tokenizer(from_hashmaps=True, encoder_hash_map=encoder_hash_map, decoder_hash_map=decoder_hash_map)
+            try:
+                self.tokenizer = naive_tokenizer(from_hashmaps=True, encoder_hash_map=encoder_hash_map, decoder_hash_map=decoder_hash_map)
+            except Exception as e:
+                raise RuntimeError("Could not load naive tokenizer, please check `encoding_hashmap.json`, `decoding_hashmap.json` &  `characters.json` provided.") from e
 
         # Use tiktokens
-        else:
-            pass
+        elif model_params['tokenizer_type'] == "tiktoken":
+            try: 
+                self.tokenizer = tiktokenizer(encoding=model_params['tokenizer_encoding'])
+            except Exception as e:
+                raise RuntimeError("Could not load tiktokenizer, please check params `tokenizer_type` & `tokenizer_encoding` in `params.json`") from e
 
-        # Load the model Language model
-        if not set(os.listdir(os.path.join(run_dir, 'model'))).issuperset(set(['BigramModel.pt', 'Optimizer.pt', 'params.json'])):
-            raise FileNotFoundError("To use language model 'BigramModel.pt', 'Optimizer.pt' & 'params.json' are needed for initialising the pytorch model")
-        
-        # Else check the device
-        elif device not in devices:
+        # Else tokenizer could not be loaded
+        else:
+            raise RuntimeError("Could not load Tokenizer")
+
+        # Check the device
+        if device not in devices:
             raise RuntimeError(f"Processing engine not found please set device to one of the following options {devices}")
         
         # Set device
         self.device = device
 
-        # Get model params as dictionary
-        with open(os.path.join(run_dir, "model", "params.json")) as f:
-            model_params = json.load(f)
-
-        self.model_params = model_params
+        # Load the model Language model
+        if not set(os.listdir(os.path.join(run_dir, 'model'))).issuperset(set(['BigramModel.pt', 'Optimizer.pt', 'params.json'])) and not set(os.listdir(os.path.join(run_dir, 'model'))).issuperset(set(['LanguageModel.pt', 'Optimizer.pt', 'params.json'])):
+            raise FileNotFoundError("To use language model 'BigramModel.pt', 'Optimizer.pt' & 'params.json' are needed for initialising the pytorch model")
+        
 
         # Load model on specified device and get the model params
-        model_path = os.path.join(run_dir, "model", "BigramModel.pt")
-        self.model = LanguageModel(vocab_size=len(encoder_hash_map.keys()), 
-                                block_size=model_params['block_size'], 
-                                n_embedd=model_params['n_embedd'],
-                                device=device,
-                                attention_head_size=model_params['attention_head_size'],
-                                num_heads= model_params['num_heads'],
-                                num_layers=model_params['num_layers'],
-                                dropout = 0
-                                )
-        self.model.load_state_dict(torch.load(model_path, weights_only=True, map_location=device))
-        self.model = self.model.to(device=device)
+        elif set(os.listdir(os.path.join(run_dir, 'model'))).issuperset(set(['BigramModel.pt', 'Optimizer.pt', 'params.json'])):
+            model_path = os.path.join(run_dir, "model", "BigramModel.pt")
+            self.model = LanguageModel(vocab_size=len(encoder_hash_map.keys()), 
+                                    block_size=model_params['block_size'], 
+                                    n_embedd=model_params['n_embedd'],
+                                    device=device,
+                                    attention_head_size=model_params['attention_head_size'],
+                                    num_heads= model_params['num_heads'],
+                                    num_layers=model_params['num_layers'],
+                                    dropout = 0
+                                    )
+            self.model.load_state_dict(torch.load(model_path, weights_only=True, map_location=device))
+            self.model = self.model.to(device=device)
+
+        else:
+            model_path = os.path.join(run_dir, "model", "LanguageModel.pt")
+            self.model = LanguageModel(vocab_size=len(encoder_hash_map.keys()), 
+                                    block_size=model_params['block_size'], 
+                                    n_embedd=model_params['n_embedd'],
+                                    device=device,
+                                    attention_head_size=model_params['attention_head_size'],
+                                    num_heads= model_params['num_heads'],
+                                    num_layers=model_params['num_layers'],
+                                    dropout = 0
+                                    )
+            self.model.load_state_dict(torch.load(model_path, weights_only=True, map_location=device))
+            self.model = self.model.to(device=device)
 
     def generate(self, text:str = "Hi dear model", max_tokens: int = 512):
         """
