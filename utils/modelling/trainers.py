84558ea059108a3f4a53fb8755375273d964fb1b
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-11-12 12:46:20 +0530

    ADDED: Docstring

--- utils/modelling/trainers.py
+++ utils/modelling/trainers.py
@@ -2,8 +2,19 @@
 import torch
 from utils.data.augmenters import batch_generator
 from tqdm import tqdm
+
+
 def naive_trainer(data: torch.Tensor, model: torch.nn.Module, optimizer, batch_size: int, block_size: int, steps: int = 100):
-    
+    """
+    Function: Basic training loop that trains a pytorch module using an optimizer and batch size for n-steps
+    Args:
+        data (torch.Tensor): Tokenized data array
+        model (torch.nn.Module): Decoder only transformer model to train
+        optimizer : Torch optimizer object used for training the model
+        block_size (int): Block size is the maximum context window of the model
+        batch_size (int): Batch size is the number of concurrent samples we can load for GPU saturation
+        steps (int) : The number of steps to train the model for
+    """
     # Check device 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
