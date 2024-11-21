ab86aa558defe12fe94249247ef7eca1b3cd97e8
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-11-21 13:34:39 +0530

    NEW RUNS

--- sandbox/sandbox-inference-pipeline.py
+++ sandbox/sandbox-inference-pipeline.py
@@ -1,6 +1,6 @@
 from utils.pipelines.inference  import InferencePipeline
 
-pipe = InferencePipeline("./runs/run-0012", device="cuda:0")
+pipe = InferencePipeline("./runs/run-0016/checkpoints", device="cuda:0")
 prompt = "How many states in Australia? "
 res = pipe.generate(prompt, max_tokens=150)
 
