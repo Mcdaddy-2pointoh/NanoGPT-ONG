7b44d7f711abfb7423da91b4f88f359f8e006b34
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2024-12-05 11:10:39 +0530

    BUG FIX: Import statements

--- sandbox/sandbox-inference-pipeline.py
+++ sandbox/sandbox-inference-pipeline.py
@@ -1,7 +1,9 @@
-from pipelines.inference  import InferencePipeline
+from pipelines.inference.inference  import InferencePipeline
 
-pipe = InferencePipeline("./runs/run-0016/checkpoints", device="cuda:0")
-prompt = "How many states in Australia? "
+pipe = InferencePipeline("./runs/run-0017", device="cuda:0")
+prompt = """There are other relevant issues in this whole debate, among which are serious concerns at the alarmingly slow pace of the destruction of weaponized chemical agents by the major possessor States."""
 res = pipe.generate(prompt, max_tokens=150)
 
-print(prompt, res)
+print(prompt)
+print()
+print(res)
