9ad3b118c641809cd2f5f2b7befc7b1e6c534eb8
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2025-01-06 16:40:33 +0530

    UPDATED: `trainer.py` to inclued mlflow tracking

--- trainers/lazy_batch_trainer/trainer.py
+++ trainers/lazy_batch_trainer/trainer.py
@@ -1,11 +1,13 @@
 import torch
 import os
 from tqdm import tqdm
+import mlflow
+import mlflow.pytorch
 from trainers.lazy_batch_trainer.switch import switch
 import numpy as np
 from data_processing.augmenters import train_test_splitter, batch_generator
 
-def lazy_batch_trainer(dir_path: str, model: torch.nn.Module, optimizer, batch_size: int, block_size: int, steps: int, check_point_params: dict = None, device: str = 'cpu', train_ratio: float = 0.90):
+def lazy_batch_trainer(dir_path: str, model: torch.nn.Module, optimizer, batch_size: int, block_size: int, steps: int, check_point_params: dict = None, device: str = 'cpu', train_ratio: float = 0.90, model_params={}):
     """
     Function: Trains the model in parts upon each of the file in dir_path
     Args:
@@ -15,9 +17,10 @@ def lazy_batch_trainer(dir_path: str, model: torch.nn.Module, optimizer, batch_s
         block_size (int): Block size is the maximum context window of the model
         batch_size (int): Batch size is the number of concurrent samples we can load for GPU saturation
         steps (int) : The number of steps to train the model for 
-        check_pointing (dict): Dictionary contains information about the checkpointing the model
+        check_point_params (dict): Dictionary contains information about the checkpointing the model
         device (str) : Set the computational device  
         train_ratio (0 < float < 1): The ratio of data to be considered as training data
+        model_params (dict): Captures all the model training params
     """
 
     # Create a dictionary to validate the coverage of data used
@@ -64,6 +67,7 @@ def lazy_batch_trainer(dir_path: str, model: torch.nn.Module, optimizer, batch_s
             cummulated_loss = []
             training_segment = None
             checkpoint_idx = str(1).zfill(5)
+            track_via_mlflow = False
 
             # Checkpointing validation
             if check_point_params is not None:                
@@ -95,68 +99,176 @@ def lazy_batch_trainer(dir_path: str, model: torch.nn.Module, optimizer, batch_s
 
                     except Exception as e:
                         raise RuntimeError("Could not create checkpoint directories") from e
-
-            for step in tqdm(range(steps)):
-                
-                # Select a new segment to train upon
-                if step % change_frequency == 0:
-
-                    # Updated status dictionary
-                    status_dictionary, training_segment = switch(status_dictionary=status_dictionary,
-                                              loss=loss,
-                                              previous_segment=training_segment)
                     
-                    print(training_segment)
-
-                    # Training Segment is None implying no more data left
-                    if training_segment is None:
-                        break
+                # Set MLflow tracking URI 
+                elif "log_to_mlflow" in check_point_params.keys() and check_point_params['log_to_mlflow']:
+                    track_via_mlflow = True
+                    mlflow.set_tracking_uri(check_point_params['mlflow_tracking_uri'])
+                    mlflow.set_experiment(check_point_params['mlflow_experiment_name'])
                     
-                    # Set new training loss to data
-                    loss = []
+            # Start an ML flow run
+            if track_via_mlflow: 
+                with mlflow.start_run() as run:
 
-                    # Load the numpy array
-                    data_path = os.path.join(dir_path, training_segment)
-                    data_arr = list(np.load(data_path))
+                    # Log all model params and Hyperparameters
+                    for k in model_params.keys():
+                            mlflow.log_param(str(k), model_params[k])
 
-                    # Create train test split
-                    train_data, test_data = train_test_splitter(data=data_arr, split_ratio=train_ratio)
+                    # LLM training loop
+                    for step in tqdm(range(steps)):
+                        
+                        # Select a new segment to train upon
+                        if step % change_frequency == 0:
 
-                # Checkpoint the model
-                if check_point_params is not None and (step % check_point_params['save_steps'] == 0 and not step == 0):
+                            # Updated status dictionary
+                            status_dictionary, training_segment = switch(status_dictionary=status_dictionary,
+                                                    loss=loss,
+                                                    previous_segment=training_segment)
+                            
+                            print(training_segment)
 
-                    # Validation loss 
-                    model.eval()
+                            # Training Segment is None implying no more data left
+                            if training_segment is None:
+                                break
+                            
+                            # Set new training loss to data
+                            loss = []
 
-                    # Save model and optimizers
-                    torch.save(model.state_dict(), f"{check_point_params['save_dir']}/LanguageModel-checkpoint-{checkpoint_idx}.pt")
-                    torch.save(optimizer.state_dict(), f"{check_point_params['save_dir']}/Optimizer-checkpoint-{checkpoint_idx}.pt")
+                            # Load the numpy array
+                            data_path = os.path.join(dir_path, training_segment)
+                            data_arr = list(np.load(data_path))
 
-                    model.train()
+                            # Create train test split
+                            train_data, test_data = train_test_splitter(data=data_arr, split_ratio=train_ratio)
 
-                    # Set checkpoint to next idx
-                    checkpoint_idx = str(int(checkpoint_idx) + 1).zfill(5)
+                        # Checkpoint the model
+                        if check_point_params is not None and (step % check_point_params['save_steps'] == 0 and not step == 0):
 
-                # Get batch from batch loader
-                try:
-                    xb, yb = batch_generator(data=train_data, block_size=block_size, batch_size=batch_size, as_torch_tensors=True, device=device)
-                    xb, yb  = xb.to(device=device), yb.to(device=device)
+                            # Validation loss 
+                            model.eval()
 
-                except Exception as e:
-                    raise RuntimeError("Could not generate a batch from data.") from e
+                            # Evaluation function
+
+                            # Save model and optimizers
+                            torch.save(model.state_dict(), f"{check_point_params['save_dir']}/LanguageModel-checkpoint-{checkpoint_idx}.pt")
+                            torch.save(optimizer.state_dict(), f"{check_point_params['save_dir']}/Optimizer-checkpoint-{checkpoint_idx}.pt")
+
+                            model.train()
+
+                            # Set checkpoint to next idx
+                            checkpoint_idx = str(int(checkpoint_idx) + 1).zfill(5)
+
+                        # Get batch from batch loader
+                        try:
+                            xb, yb = batch_generator(data=train_data, block_size=block_size, batch_size=batch_size, as_torch_tensors=True, device=device)
+                            xb, yb  = xb.to(device=device), yb.to(device=device)
+
+                        except Exception as e:
+                            raise RuntimeError("Could not generate a batch from data.") from e
+                        
+                        # Train the model
+                        logits, batch_loss = model(xb, yb)
+                        optimizer.zero_grad(set_to_none=True)
+                        
+                        # Compute loss
+                        batch_loss.backward()
+
+                        # Take a optimizer step
+                        optimizer.step()
+
+                        # Save loss
+                        loss.append(batch_loss.item())
+                        cummulated_loss.append(batch_loss.item())
+
+                        # Extract the LR
+                        current_lr = optimizer.param_groups[0]['lr'] 
+
+                        # Log LR and loss to ML flow
+                        mlflow.log_metric("step_training_loss", batch_loss.item())
+                        mlflow.log_metric("step_learning_rate", current_lr)
+
+                    # Log the model to mlflow
+                    mlflow.pytorch.log_model(model, check_point_params['model_name'])
+                    mlflow.log_artifact(dir_path, check_point_params['dataset_name'])
+
+                    return model, cummulated_loss
+
+            # Run without ML FLOW   
+            else:
                 
-                # Train the model
-                logits, batch_loss = model(xb, yb)
-                optimizer.zero_grad(set_to_none=True)
-                batch_loss.backward()
-                optimizer.step()
-                loss.append(batch_loss.item())
-                cummulated_loss.append(batch_loss.item())
-
-            return model, cummulated_loss
-
-                
-
-
-
+                # Run training loop
+                for step in tqdm(range(steps)):
                     
+                    # Select a new segment to train upon
+                    if step % change_frequency == 0:
+
+                        # Updated status dictionary
+                        status_dictionary, training_segment = switch(status_dictionary=status_dictionary,
+                                                loss=loss,
+                                                previous_segment=training_segment)
+                        
+                        print(training_segment)
+
+                        # Training Segment is None implying no more data left
+                        if training_segment is None:
+                            break
+                        
+                        # Set new training loss to data
+                        loss = []
+
+                        # Load the numpy array
+                        data_path = os.path.join(dir_path, training_segment)
+                        data_arr = list(np.load(data_path))
+
+                        # Create train test split
+                        train_data, test_data = train_test_splitter(data=data_arr, split_ratio=train_ratio)
+
+                    # Checkpoint the model
+                    if check_point_params is not None and (step % check_point_params['save_steps'] == 0 and not step == 0):
+
+                        # Validation loss 
+                        model.eval()
+
+                        # Evaluation function
+
+                        # Save model and optimizers
+                        torch.save(model.state_dict(), f"{check_point_params['save_dir']}/LanguageModel-checkpoint-{checkpoint_idx}.pt")
+                        torch.save(optimizer.state_dict(), f"{check_point_params['save_dir']}/Optimizer-checkpoint-{checkpoint_idx}.pt")
+
+                        model.train()
+
+                        # Set checkpoint to next idx
+                        checkpoint_idx = str(int(checkpoint_idx) + 1).zfill(5)
+
+                    # Get batch from batch loader
+                    try:
+                        xb, yb = batch_generator(data=train_data, block_size=block_size, batch_size=batch_size, as_torch_tensors=True, device=device)
+                        xb, yb  = xb.to(device=device), yb.to(device=device)
+
+                    except Exception as e:
+                        raise RuntimeError("Could not generate a batch from data.") from e
+                    
+                    # Train the model
+                    logits, batch_loss = model(xb, yb)
+                    optimizer.zero_grad(set_to_none=True)
+                    
+                    # Compute loss
+                    batch_loss.backward()
+
+                    # Take a optimizer step
+                    optimizer.step()
+
+                    # Save loss
+                    loss.append(batch_loss.item())
+                    cummulated_loss.append(batch_loss.item())
+
+                    # Extract the LR
+                    current_lr = optimizer.param_groups[0]['lr'] 
+
+                return model, cummulated_loss
+
+            
+
+
+
+                
