834e078b210c0e6dc1213347acb6e2a80dcc8dae
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2025-01-16 14:44:44 +0530

    ADDED: LR visualiser

--- telemetry/visualisers.py
+++ telemetry/visualisers.py
@@ -12,12 +12,10 @@ def plot_loss(losses, dir_path, smoothen=False):
     # Check if directory exists at path 
     if not os.path.isdir(dir_path):
         raise FileNotFoundError(f"No directory found at {dir_path}")
-    else:
-        run_number = str(len(os.listdir(path=dir_path)) + 1).zfill(4)
 
     # If smoothen
     if smoothen:
-        losses = estimate_loss(losses=losses)
+        losses = estimate_loss(dist=losses)
 
     try: 
         plt.plot(list(range(1,len(losses)+1)), losses)
@@ -26,16 +24,38 @@ def plot_loss(losses, dir_path, smoothen=False):
     except Exception as e:
         raise RuntimeError("Could not save loss curve") from e
     
-def estimate_loss(losses, skip: int = None):
+def plot_lr(lr, dir_path, smoothen=False):
     """
-    Function: Smoothens the losses over `skip` steps
+    Function: Plots and saves the loss as an image per run
     Args:
         losses(list | torch.Tensor) : list of loss per step
+        dir_path(str) : to save the image 
+    """
+    # Check if directory exists at path 
+    if not os.path.isdir(dir_path):
+        raise FileNotFoundError(f"No directory found at {dir_path}")
+
+    # If smoothen
+    if smoothen:
+        lr = estimate_loss(dist=lr)
+
+    try: 
+        plt.plot(list(range(1,len(lr)+1)), lr)
+        plt.savefig(os.path.join(dir_path, f"lr.png"))
+
+    except Exception as e:
+        raise RuntimeError("Could not save learning rate curve") from e
+    
+def estimate_loss(dist, skip: int = None):
+    """
+    Function: Smoothens the dist over `skip` steps
+    Args:
+        dist(list | torch.Tensor) : list of loss per step
         skip (None | int): Smoothening window
     """
     if skip is None:
-        skip = int(len(losses)/100)
+        skip = int(len(dist)/100)
 
-    ret = np.cumsum(losses, dtype=float)
+    ret = np.cumsum(dist, dtype=float)
     ret[skip:] = ret[skip:] - ret[:-skip]
     return ret[skip - 1:] / skip
